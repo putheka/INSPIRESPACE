8f5d6665d2afe0762d2380e529c4c0d4b2d636d6
Chhon Putheka <chhonputheka@gmail.com>
2024-01-11 10:24:41 +0700

    working on User management fix update bug

--- src/component/UserModel.js
+++ src/component/UserModel.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { ThreeCircles } from 'react-loader-spinner';
 import { toast } from 'react-toastify';
 import { CREATE_USER, UPDATE_USER_BY_ID, UPLOAD_FILE } from '../services/UserService';
@@ -14,6 +16,7 @@ const UserModal = ({ updatedUser, showUser, handleCloseUserForm }) => {
   const [avatar, setAvatar] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [previewImage, setPreviewImage] = useState('');
+  const [selectedFile, setSelectedFile] = useState(null);
 
   useEffect(() => {
     if (updatedUser) {
@@ -27,60 +30,160 @@ const UserModal = ({ updatedUser, showUser, handleCloseUserForm }) => {
       setName('');
       setPassword('');
       setRole('');
-      setAvatar();
+      setAvatar('');
     }
   }, [updatedUser]);
 
   const handleUserClose = () => {
     handleCloseUserForm();
   };
-  
-  const handleCreateOrUpdateUser = async () => {
-    setIsLoading(true);
 
-    try {
-      let avatarUrl = updatedUser?.avatar || '';
+  // const handleCreateOrUpdateUser = async () => {
+  //   setIsLoading(true);
 
-      if (avatar) {
-        const fileData = new FormData();
-        fileData.append('file', avatar);
+  //   try {
+  //     let avatarUrl = updatedUser?.avatar || '';
 
-        const uploadResponse = await UPLOAD_FILE(fileData);
-        avatarUrl = uploadResponse.location;
-      }
+  //     if (avatar) {
+  //       const fileData = new FormData();
+  //       fileData.append('file', avatar);
 
-      const userData = { email, name, password, role, avatar: avatarUrl };
+  //       const uploadResponse = await UPLOAD_FILE(fileData);
+  //       avatarUrl = uploadResponse.location;
+  //     }
 
-      if (updatedUser) {
-        await UPDATE_USER_BY_ID(updatedUser.id, userData);
-        toast.success('Updated User Successfully!');
-      } else {
-        await CREATE_USER(userData);
-        toast.success('Created User Successfully!');
-      }
-      setIsLoading(false);
-      handleCloseUserForm();
-    } catch (error) {
-      console.error('Error:', error);
-      toast.error('Operation failed!');
-      setIsLoading(false);
-    }
-  };
+  //     const userData = { 
+  //       email,
+  //       name,
+  //       password,
+  //       role,
+  //       avatar: avatarUrl };
+
+  //     if (updatedUser) {
+  //       await UPDATE_USER_BY_ID(updatedUser.id, userData);
+  //       toast.success('Updated User Successfully!');
+  //     } else {
+  //       await CREATE_USER(userData);
+  //       toast.success('Created User Successfully!');
+  //     }
+  //     setIsLoading(false);
+  //     handleCloseUserForm();
+  //   } catch (error) {
+  //     console.error('Error:', error);
+  //     toast.error('Operation failed!');
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  // const handleImageChange = (e) => {
+  //   const file = e.target.files[0];
+
+  //   if (file) {
+  //     const reader = new FileReader();
+
+  //     reader.onloadend = () => {
+  //       setAvatar(file);
+  //       setPreviewImage(reader.result);
+  //     };
+
+  //     reader.readAsDataURL(file);
+  //   }
+  // };
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
 
     if (file) {
-      const reader = new FileReader();
-
-      reader.onloadend = () => {
-        setAvatar(file);
-        setPreviewImage(reader.result);
-      };
-
-      reader.readAsDataURL(file);
+      setSelectedFile(file);
+      const imageUrl = URL.createObjectURL(file);
+      setPreviewImage(imageUrl);
     }
   };
+
+  const handleCreateOrUpdateUser = () => {
+    setIsLoading(true);
+
+    const userData = {
+      email,
+      name,
+      password,
+      role,
+      avatar: updatedUser ? updatedUser.avatar : '',
+    };
+
+    if (selectedFile) {
+      const fileData = new FormData();
+      fileData.append('file', selectedFile);
+
+      UPLOAD_FILE(fileData)
+        .then((response) => {
+          userData.avatar = response.location;
+
+          if (updatedUser) {
+            // Update user
+            UPDATE_USER_BY_ID(updatedUser.id, userData)
+              .then(() => {
+                toast.success('Updated User Successfully!');
+                setIsLoading(false);
+                handleCloseUserForm();
+              })
+              .catch((error) => {
+                console.error('Error updating user', error);
+                toast.error('Failed to update user!');
+                setIsLoading(false);
+              });
+          } else {
+            // Create user
+            CREATE_USER(userData)
+              .then(() => {
+                toast.success('Created User Successfully!');
+                setIsLoading(false);
+                handleCloseUserForm();
+              })
+              .catch((error) => {
+                console.error('Error creating user', error);
+                toast.error('Failed to create user!');
+                setIsLoading(false);
+              });
+          }
+        })
+        .catch((error) => {
+          console.error('Error uploading file', error);
+          toast.error('Failed to upload file!');
+          setIsLoading(false);
+        });
+    } else {
+      // No file selected
+      if (updatedUser) {
+        // Update user without changing the avatar
+        UPDATE_USER_BY_ID(updatedUser.id, userData)
+          .then(() => {
+            toast.success('Updated User Successfully!');
+            setIsLoading(false);
+            handleCloseUserForm();
+          })
+          .catch((error) => {
+            console.error('Error updating user', error);
+            toast.error('Failed to update user!');
+            setIsLoading(false);
+          });
+      } else {
+        // Create user without an avatar
+        CREATE_USER(userData)
+          .then(() => {
+            toast.success('Created User Successfully!');
+            setIsLoading(false);
+            handleCloseUserForm();
+          })
+          .catch((error) => {
+            console.error('Error creating user', error);
+            toast.error('Failed to create user!');
+            setIsLoading(false);
+          });
+      }
+    }
+  };
+
 
   return (
     <Modal size="lg" show={showUser} onHide={handleUserClose}>
@@ -148,7 +251,7 @@ const UserModal = ({ updatedUser, showUser, handleCloseUserForm }) => {
                 />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formRole">
+              {/* <Form.Group className="mb-3" controlId="formRole">
                 <Form.Label>Role</Form.Label>
                 <Form.Control
                   type="text"
@@ -156,7 +259,20 @@ const UserModal = ({ updatedUser, showUser, handleCloseUserForm }) => {
                   value={role}
                   onChange={(e) => setRole(e.target.value)}
                 />
+              </Form.Group> */}
+              <Form.Group className="mb-3" controlId="formRole">
+                <Form.Label>Role</Form.Label>
+                <Form.Control
+                  as="select"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                >
+                  <option value="customer">Customer</option>
+                  <option value="admin">Admin</option>
+                </Form.Control>
               </Form.Group>
+
+
             </Form>
           </div>
         </div>
